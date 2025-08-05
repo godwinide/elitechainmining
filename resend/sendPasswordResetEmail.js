2ebd5cc98fbbf6ba8b89c6de9eb93794a7cb9021
iamgodwinide <iamgodwinide@gmail.com>
2025-08-05 01:13:45 +0100

    new

--- resend/sendPasswordResetEmail.js
+++ resend/sendPasswordResetEmail.js
@@ -4,14 +4,14 @@ async function sendPasswordResetEmail(receipient, linkId){
     const resend = new Resend(process.env.RESEND_API_KEY);
     await resend.emails.send({
         headers: {
-            "X-My-Header": "Musk Space"
+            "X-My-Header": "EliteChainMining"
         },
-        from: "Musk Space <noreply@spacexmusk.icu>",
+        from: "EliteChainMining <noreply@elitechainmining.pro>",
         subject: "Reset Password",
         to: [receipient],
         html: `
             <p>Click the link below to reset your password:</p>
-            <p><a href="https://spacexmusk.icu/reset-password?code=${linkId}">Reset Password</a></p>
+            <p><a href="https://elitechainmining.pro/reset-password?code=${linkId}">Reset Password</a></p>
             <p>Thank you for using Musk Space!</p>
             `
       });
